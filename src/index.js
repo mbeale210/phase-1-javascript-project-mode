d2a48c902bd9bb89c6f8bd37892f113b0261f10c
Mark Beale <bealebeaverst@gmail.com>
2024-07-29 14:19:15 -0700

    const defined and API link added

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // our javascript code goes here
+const API = "file://wsl.localhost/Ubuntu/home/mbeale/Development/code/phase-1/phase-1-javascript-project-mode/db.json"
+const godsDiv = document.getElementById("display");
 
+const displayGods = () => {
+  fetch(API)
+    .then((res) => res.json())
+    .then(godsInfo);
 
-
+};
 // db.json populated from api source:https://github.com/betalantz/json-server-collection/blob/main/greek-mythology/db.json
